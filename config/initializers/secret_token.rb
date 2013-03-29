dd80595656b48aec31e48a71bbcf3b6aaf4bf238
Mark Ryall <mark@ryall.name>
2013-03-29 10:00:30 +1000

    empty rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schatter::Application.config.secret_token = 'e485e19cc93d63a82c3012908ba2d5e51460d087712a29a859c8e2795d99d6388999eaa048f259b5f76f3521673772fb7cd1cfe192cfb8297b5568028a6ef9c1'
