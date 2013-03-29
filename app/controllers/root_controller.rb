2ea9d2de27390a81b71df494a5784abfffd3b256
Mark Ryall <mark@ryall.name>
2013-03-29 17:36:48 +1000

    added a simplistic RESTful style json response to root url

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,10 +2,22 @@ class RootController < ApplicationController
   skip_filter :require_login
 
   def index
-    if logged_in?
-      redirect_url = session[:initial_url] || home_url
-      session[:initial_url] = nil
-      redirect_to redirect_url
+    respond_to do |format|
+      format.html do
+        if logged_in?
+          redirect_url = session[:initial_url] || home_url
+          session[:initial_url] = nil
+          redirect_to redirect_url
+        end
+      end
+      format.json do
+        render json: {
+          _links: {
+            self: { href: root_url },
+            conversations: { href: conversation_index_url }
+          }
+        }
+      end
     end
   end
 
