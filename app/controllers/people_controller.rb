834d2a29d66fea55597c79ad1404a8bc7851b453
Mark Ryall <mark@ryall.name>
2013-04-01 09:01:35 +1000

    fixed join conversation

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,8 +2,7 @@ class PeopleController < ApplicationController
   def create
     with_conversation params[:conversation_id] do |conversation|
       person = Person.find_or_create_for_email params[:email]
-      person.join_conversation
-      message = current_person.create_message conversation, params[:content]
+      person.join_conversation conversation unless conversation.people.include?(person)
       render json: person, status: 201
     end
   end
